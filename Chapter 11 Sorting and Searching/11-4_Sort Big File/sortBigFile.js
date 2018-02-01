6bf3e96057c00adae7d22a7b93ec8aeda43a0882
NinaSleephead <kx1214@gmail.com>
2018-02-01 09:34:13 -0700

    Sort Big File

---
+++ Chapter 11 Sorting and Searching/11-4_Sort Big File/sortBigFile.js
@@ -0,0 +1,5 @@
+//Divide the file into chunks which are x megabytes each
+//Each chunk is sorted separeatly
+//save chunks back to the file system
+//All the chunks are sorted
+//Merge the chunks one by one
